7c1a6efefeba315f9dcb34ddaa548f290609bb81
RocioCellini <rociocllini@gmail.com>
2021-12-26 18:38:41 -0300

    Sección 4 - Cap 41

---
+++ 03-counter-app/src/CounterApp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({value}) => {
+    return (
+        <>
+        <h1>CounterApp</h1>
+        <h2> { value } </h2>
+        </>
+        
+    );
+    CounterApp.propTypes = {
+        value: PropTypes.number
+    }
+    
+}
+
+
+export default CounterApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -3,10 +3,11 @@
 
 import React from 'react';
 import ReactDom from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+//import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 
 import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDom.render( <PrimeraApp saludo="Hola Mundo" />, divRoot);
+ReactDom.render( <CounterApp value = {123} />, divRoot);
