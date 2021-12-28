4243d5ba3a1330221d7b11db681d3441ba2e4174
RocioCellini <rociocllini@gmail.com>
2021-12-28 16:33:39 -0300

    Sección 4 - Cap 44

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -8,7 +8,7 @@ const CounterApp = ({value}) => {
     const [nombre, setnombre]= useState('Goku');
     console.log(nombre, setnombre);*/
 
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(value);
 
     const handleAdd = () => {
         setCounter(counter + 1);
@@ -16,13 +16,23 @@ const CounterApp = ({value}) => {
         setCounter((c)=>c+1);
         */
     }
+
+    /* al ser de una linea no hacen falta las llaves en la función */
+    const handleSustract = () => setCounter(counter - 1);
+    
+    const handleReset = () => {
+        setCounter(value);
+    }
+
     return (
         <>
-        <h1>CounterApp</h1>
-        <h2> { counter } </h2>
-     {/*  también puedo colocar en lugar de definir la función arriba
-      <button onClick={ (e) =>{console.log(e)} }>+1</button>  */}
-     <button onClick={ handleAdd }>+1</button>
+            <h1>CounterApp</h1>
+            <h2> { counter } </h2>
+            {/*  también puedo colocar en lugar de definir la función arriba
+            <button onClick={ (e) =>{console.log(e)} }>+1</button>  */}
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSustract }>-1</button>
         </>
         
     );
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -10,4 +10,4 @@ import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDom.render( <CounterApp value = {123} />, divRoot);
+ReactDom.render( <CounterApp value = {10} />, divRoot);
