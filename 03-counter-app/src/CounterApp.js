5ed2ca42c4ebcc19815b651a877f99a96aadac1e
RocioCellini <rociocllini@gmail.com>
2021-12-28 15:16:08 -0300

    Sección 4 - Cap 43

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,15 +1,25 @@
 /* eslint-disable no-unreachable */
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
-    const handleAdd = (e) => {
-        console.log(e)
+   /*  const state= useState('Goku');
+    console.log(state); 
+    const [nombre, setnombre]= useState('Goku');
+    console.log(nombre, setnombre);*/
+
+    const [counter, setCounter] = useState(0);
+
+    const handleAdd = () => {
+        setCounter(counter + 1);
+        /* otra forma de incrementar seria:
+        setCounter((c)=>c+1);
+        */
     }
     return (
         <>
         <h1>CounterApp</h1>
-        <h2> { value } </h2>
+        <h2> { counter } </h2>
      {/*  también puedo colocar en lugar de definir la función arriba
       <button onClick={ (e) =>{console.log(e)} }>+1</button>  */}
      <button onClick={ handleAdd }>+1</button>
