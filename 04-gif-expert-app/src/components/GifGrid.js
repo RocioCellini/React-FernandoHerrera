bb58f0fc895cc539fa7df559544b515fb5201759
RocioCellini <rociocllini@gmail.com>
2022-01-10 18:23:00 -0300

    Sección 6 - Cap 74

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const GifGrid = ({category}) => {
+
+    const [count, setCount] = useState(0);
+
+    /*useEffect evitará que se entre en un bucle infinito cada vez q se renderice el componente. 
+    Hace que solo se llame a getGifs la primera vez q se carga el componente */
+    
+    useEffect(()=>{
+        getGifs();
+    },[])
 
     const getGifs = async() => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=harry+potter&limit=10&api_key=3dT4JSMP4Lp9oORwbHgEAzO3Jp3MrR18';
@@ -18,11 +27,13 @@ export const GifGrid = ({category}) => {
         console.log(gifs);
     }
 
-    getGifs();
+   // getGifs();
 
     return(
         <div>
         <h3> {category} </h3>
+        <h3> {count} </h3>
+        <button onClick={()=>setCount(count +1)}>Sumar</button>
         </div>
     )
 }
