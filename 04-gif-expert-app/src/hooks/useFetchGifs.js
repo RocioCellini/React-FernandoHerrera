cebfe46dcce9ac7a37eb6811ace0988084ad8e8c
RocioCellini <rociocllini@gmail.com>
2022-01-22 15:21:45 -0300

    Sección 6 - Cap 80

--- 04-gif-expert-app/src/hooks/useFetchGifs.js
+++ 04-gif-expert-app/src/hooks/useFetchGifs.js
@@ -10,12 +10,10 @@ export const useFetchGifs = (category) => {
     useEffect(()=>{
         getGifs(category) //como getGifs es una promesa puedo usar then
         .then( imgs =>{
-            setTimeout(()=>{
-                setState({
-                    data: imgs,
-                    loading: false
-                });
-            }, 3000);
+            setState({
+                data: imgs,
+                loading: false
+            });
         }) // setImages da el mismo resultado que poner imgs => setImages(imgs)
     },[category]) //el effect hace q suceda lo q contiene cada vez q cambia la categoría
 
