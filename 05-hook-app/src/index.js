63d3349ff632c1c3f49af3ecf8d4b2d9d7b973a7
RocioCellini <rociocllini@gmail.com>
2022-02-09 23:59:26 -0300

    Sección 9 - Cap 106

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HookApp } from './HookApp';
+import { CounterApp } from './components/01-useState/CounterApp';
+//import { HookApp } from './HookApp';
 
 
 ReactDOM.render(
-    <HookApp />,
+    <CounterApp />,
   document.getElementById('root')
 );
 
