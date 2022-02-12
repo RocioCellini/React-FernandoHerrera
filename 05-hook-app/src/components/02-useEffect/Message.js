5cbf8891c5f7c064e85021beda890289444576fa
RocioCellini <rociocllini@gmail.com>
2022-02-11 21:33:31 -0300

    Sección 9 - Cap 110

--- 05-hook-app/src/components/02-useEffect/Message.js
+++ 05-hook-app/src/components/02-useEffect/Message.js
@@ -1,17 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({x:0,y:0})
+    const {x, y} = coords;
+
     useEffect(()=>{
-        console.log('componente montado')
+        const mouseMove = (e)=>{
+           //console.log(':D');
+           const coors = {x:e.x, y:e.y};
+           // console.log(coors); 
+           setCoords(coors);
+        }
+
+        window.addEventListener('mousemove', mouseMove);
+
         return () =>{
-            console.log('componente desmontado')
+            //console.log('componente desmontado')
+            window.removeEventListener('mousemove', mouseMove);
         } 
     },[])
 
     return(
         <div>
             <h3>eres genial!</h3>
+            <p>x: {x} , y: {y}</p>
         </div>
     )
 }
