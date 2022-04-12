c111b70d66184857ccf77b7cf23c926fa3426dba
Rocio <rociocellini@gmail.com>
2022-04-12 14:19:47 -0300

    Sección 10 - Cap 128

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -16,14 +16,46 @@ export const TodoApp = () => {
 
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({todos.length})</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola denuevo</li>
-            </ul>
+            <div className='row'>
+                <div className='col-7'>
+                <ul
+                className='list-group list-group-flush'
+                >
+                    {
+                        todos.map( (todo,i) =>(
+                        <li
+                            key={todo.id}
+                            className="list-group-items"
+                        > 
+                        <p> {i+1}. {todo.desc} </p>
+                        <button className='btn btn-danger'>
+                            Borrar
+                        </button>
+                        </li>
+                    ))
+                    }
+                </ul>
+                </div>
+                <div className='col-5'>
+                    <h4>Agregar TODO</h4>
+                    <hr />
+                    <form>
+                        <input
+                        type='text'
+                        name='description'
+                        className='form-control'
+                        placeholder='Aprender...'
+                        autoComplete='off'
+                        >
+                        </input>
+                        <button className='btn btn-outline-primary mt-1 btn-block'></button>
+                    </form>
+                </div>
+            </div>
+           
         </div>
     )     
 }
